81930e701703e9bcb9adbd8e6eb60505caf28ed1
lin <714613490@qq.com>
2020-05-20 22:00:11 +0800

    excel读取回写数据完成

--- src/main/java/com/lin/utils/ExcelUtill.java
+++ src/main/java/com/lin/utils/ExcelUtill.java
@@ -7,16 +7,55 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.Iterator;
 import java.util.List;
 
 public class ExcelUtill {
+    public static Map<String,Integer> caseIdRowNum=new HashMap<String,Integer>();
+    public static Map<String,Integer> cellNameCellNum=new HashMap<String,Integer>();
+    public static Workbook workbook;
+    static {
+        LoadMapping("src/main/resources/test.xlsx","type");
+    }
+    private static void LoadMapping(String Path,String sheetName){
+        InputStream inputStream=null;
+        try {
+            inputStream=new FileInputStream(Path);
+             workbook=WorkbookFactory.create(inputStream);
+            Sheet sheet=workbook.getSheet(sheetName);
+            Row first=sheet.getRow(0);
+            if(first!=null&&!isEmptyRow(first)){
+                int cellNum=first.getLastCellNum();
+                for(int i=0;i<cellNum;i++){
+                    Cell cell=first.getCell(i,Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                    cell.setCellType(CellType.STRING);
+                    String title=cell.getStringCellValue();
+                    int index=cell.getAddress().getColumn();
+                    cellNameCellNum.put(title,index);
+                }
+                for(int i=1;i<=sheet.getLastRowNum();i++){
+                    Row row=sheet.getRow(i);
+                    Cell cell= row.getCell(0,Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                    cell.setCellType(CellType.STRING);
+                    String CaseId=cell.getStringCellValue();
+                    int rowNum=row.getRowNum();
+                    caseIdRowNum.put(CaseId,rowNum);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(inputStream!=null){
+                    inputStream.close();}
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     /*   加载excel数据*/
     public static <T> List<T> load(String url,String sheetName,Class<T> clazz){
           if(null==url||"".equals(url)){
@@ -26,14 +65,14 @@ public class ExcelUtill {
         InputStream is;
         try {
             is=new FileInputStream(url);
-            if(url.endsWith(".xls")){
+            /*if(url.endsWith(".xls")){
 
                 excel = new HSSFWorkbook(is);
                 } else {
                 excel = new XSSFWorkbook(is);
                 }
-                is.close();
-            return transToObject(clazz, excel, sheetName);
+                is.close();*/
+            return transToObject(clazz, workbook, sheetName);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException("转换excel文件失败：" + e.getMessage());
@@ -80,6 +119,38 @@ public class ExcelUtill {
         }
 
          return list;
+    }
+    /*
+    * 回显数据，Map的key为caseId，value为写入数据
+    * */
+    public static <T> void save(Class<T> clazz,String url,String sheetName,Map<String,String> map,String field ){
+
+        OutputStream outputStream=null;
+        try {
+            for(Map.Entry<String,String> entry:map.entrySet()){
+                int rowNum=caseIdRowNum.get(entry.getKey());
+                int cellNum=cellNameCellNum.get(field);
+                outputStream=new FileOutputStream(url);
+
+                Sheet sheet=workbook.getSheet(sheetName);
+                Row row=sheet.getRow(rowNum);
+                Cell cell=row.getCell(cellNum,Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                cell.setCellType(CellType.STRING);
+                cell.setCellValue(entry.getValue());
+                workbook.write(outputStream);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if(outputStream!=null){
+                    outputStream.close();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }}
+
     }
     /*
     * 获取单元格的值
@@ -135,5 +206,10 @@ public class ExcelUtill {
           Case c= i.next();
            System.out.println(c);
        }
+      /* Map<String,String> map=new HashMap<String,String>();
+       map.put("1","1");
+      map.put("2","1");
+       save(Case.class,"src/main/resources/test.xlsx","type",map,"save");*/
+
   }
 }
